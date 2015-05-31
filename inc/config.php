dd2833907d1101be5fd1bb6b5fe0d1fb887b7dd9
Ted Sumner <teds@biblewordstudy.net>
2015-05-31 10:23:17 -0700

    changes to config for new location in briefcase to sync between computers

--- inc/config.php
+++ inc/config.php
@@ -1,5 +1,5 @@
 <?php
 
-define("BASE_URL", "/cub_scout/");
-define("ROOT_PATH", $_SERVER["DOCUMENT_ROOT"] . "/cub_scout/");
+define("BASE_URL", "/web_projects/cub_scout/");
+define("ROOT_PATH", $_SERVER["DOCUMENT_ROOT"] . "/web_projects/cub_scout/");
 define("GOOGLE","UA-XXXXX-X");
