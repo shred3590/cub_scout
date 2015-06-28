72fe72ccfda43be2e6b062b32fcd923232b9a553
shred3590 <teds@biblewordstudy.net>
2015-06-28 10:35:31 -0700

    This is a test database that I will use to start building the database project.  This is complicated by the nature of the projects where you do not have to complete all the steps for some of the tasks. This contains an outline of what I currently believe the structure will be.

---
+++ database_learn/index.php
@@ -0,0 +1,27 @@
+<?php
+
+// call database
+
+//list database contents with checkboxes
+
+// submit checkboxes to be stored in database
+
+// compute progress toward badge
+
+	// required
+
+		// complete all tasks
+
+			// no choices
+
+			// some choices in how to complte task.
+
+				// complete some mandatory, some elective
+
+				// complete only elective, no mandatory
+
+	// elective
+
+		// some choices tree from above? 
+
+// display child's progress toward completion of each task and badge.  Probably one of the JavaScript solutions in downloads.
